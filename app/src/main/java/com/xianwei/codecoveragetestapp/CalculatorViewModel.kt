89355a02c5292e4e6242dc0863dabbba4ff213f2
li Ethan <lixw1021@gmail.com>
2021-06-12 12:58:46 +0100

    init project

---
+++ app/src/main/java/com/xianwei/codecoveragetestapp/CalculatorViewModel.kt
@@ -0,0 +1,26 @@
+package com.xianwei.codecoveragetestapp
+
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+
+
+
+    fun onMultiplyClicked(inputA: Int, inputB: Int) {
+
+    }
+
+    fun onMinusClicked(inputA: Int, inputB: Int) {
+
+    }
+
+    fun onInputAUpdate(toString: String) {
+
+    }
+
+    fun onInputBUpdate(toString: String) {
+
+
+    }
+
+}
---
+++ app/src/main/java/com/xianwei/codecoveragetestapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.xianwei.codecoveragetestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
+import com.xianwei.codecoveragetestapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: CalculatorViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    private val inputA get() = binding.inputA.text.toString().toInt()
+    private val inputB get() = binding.inputB.text.toString().toInt()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+
+        binding.multiply.setOnClickListener {
+            if (validInput()) {
+                viewModel.onMultiplyClicked(inputA, inputB)
+                binding.sign.text = "+"
+            }
+        }
+
+        binding.minus.setOnClickListener {
+            if (validInput()) {
+                viewModel.onMinusClicked(inputA, inputB)
+                binding.sign.text = "-"
+            }
+        }
+
+        binding.multiply.setOnClickListener {
+        }
+
+        binding.multiply.setOnClickListener {
+        }
+    }
+
+    private fun validInput(): Boolean =
+        if (binding.inputA.length() > 0 && binding.inputB.length() > 0) {
+            true
+        } else {
+            Toast.makeText(baseContext, "Please input a valid value", Toast.LENGTH_SHORT).show()
+            false
+        }
+}
